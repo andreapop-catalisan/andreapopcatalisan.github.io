15b8a4ab90d98e8b9bd048e36f2b9a4db9970ab4
andreapopcatalisan <100367102@alumnos.uc3m.es>
2025-07-07 16:34:02 +0000

    Deploying to gh-pages from @ andreapopcatalisan/andreapopcatalisan.github.io@a9bf45bb34e3da34e3bace3654dd32173a30a597 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -9,8 +9,8 @@ ninja.data = [{
     handler: () => {
       window.location.href = "/";
     },
-  },{id: "nav-",
-          title: "",
+  },{id: "nav-research",
+          title: "RESEARCH",
           description: "",
           section: "Navigation",
           handler: () => {
